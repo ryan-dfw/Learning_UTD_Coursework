5a57cb33502f258146937cb9e30097deee99c7e7
gdknas <mckevitt.r@gmail.com>
2023-09-27 12:56:07 -0500

    prepared string parsing function as if else

--- block14/index.js
+++ block14/index.js
@@ -1,10 +1,34 @@
-const unsortedArray = [
+const numbersArrays = [
   [2, 4, 6, 8, 11, 20, 15, 22],
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   [70, 42, 55, 81, 21, 91, 34],
   [2, 4, 6, 8, 10, 11, 12],
 ];
 
-unsortedArray.forEach((innerArray) =>
+const stringsArray = [
+  "hello",
+  "ukulele",
+  "awesome",
+  "onomatopoeia",
+  "textbook",
+];
+
+numbersArrays.forEach((innerArray) =>
   console.log(innerArray.filter((item) => item % 2 === 1).join(", "))
 );
+
+stringsArray.forEach((string) => {
+  let consonantCount = 0;
+  let vowelCount = 0;
+
+  for (const character of string) {
+    if (/[aeiou]/i.test(character)) {
+      vowelCount++;
+    } else {
+      consonantCount++;
+    }
+  }
+  console.log(
+    `${string} has ${consonantCount} consonants and ${vowelCount} vowels`
+  );
+});
