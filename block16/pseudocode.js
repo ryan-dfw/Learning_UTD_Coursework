abcebcf8918eea00bdc70e0178293804966fc1d1
gdknas <mckevitt.r@gmail.com>
2023-10-04 15:45:03 -0500

    added pseudocode

---
+++ block16/pseudocode.js
@@ -0,0 +1,37 @@
+// let there be an immutable pointer named timmy to an array of the following properties:
+// a prescription for acetominophen
+// a price per refill of 25
+// three refills
+// no subscription
+// no coupon
+
+// let there be an immutable pointer named sarah to an array of the following properties:
+// a prescription for diphenhydramine
+// a price per refill of 50
+// one refill
+// a subscription
+// no coupon
+
+// let there be an immutable pointer named rocky to an array of the following properties:
+// a prescription for phenylephrine
+// a price per refill of 30
+// five refills
+// a subscription
+// a coupon
+
+// let there be a christening function considering a customer and their namae
+// let their name become their namae
+
+// let there be an evaluating function considering a customer
+// let cost be the price per refill by the number of refills
+// should a subscription exist, mutate the cost by .75; otherwise don't
+// should a coupon exist, mutate the cost by subtracting 10; otherwise don't
+// print to standard output their name, an explanatory message, and their evaulated cost
+
+// christen the timmy pointer by the namae Timmy
+// christen the sarah pointer by the namae Sarah
+// christen the rocky pointer by the namae Rocky
+
+// evaluate the cost of the timmy array
+// evaluate the cost of the sarah array
+// evaluate the cost of the rocky array
