4740fb252de65c928c74fa6facfcca8e5a1dd507
gdknas <mckevitt.r@gmail.com>
2023-11-23 19:41:49 -0600

    b25 added border to selected colour & whole navbar colour changing

--- block25/src/App.jsx
+++ block25/src/App.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 
-const Color = (props) => {
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const handleClick = () => {
+    setSelectedColor(color);
+  };
+
+  const isSelected = color === selectedColor;
+
+  const style = {
+    backgroundColor: color,
+    border: isSelected ? "50px solid white" : "none",
+    boxSizing: "border-box",
+  };
+
   return (
     <div
-      onClick={() => props.setSelectedColor(props.color)}
-      className={props.color}></div>
+      onClick={handleClick}
+      className={`color ${isSelected ? "selected" : ""}`}
+      style={style}></div>
   );
 };
 
 const App = () => {
   const [selectedColor, setSelectedColor] = useState("");
+  const navbarStyle = {
+    backgroundColor: selectedColor,
+  };
   return (
     <div id="container">
-      <div id="navbar">
+      <div
+        id="navbar"
+        style={{ backgroundColor: selectedColor }}>
         <div>Currently selected: </div>
         <div className={selectedColor}>{selectedColor}</div>
       </div>
@@ -20,14 +38,17 @@ const App = () => {
         <Color
           color="red"
           setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
         />
         <Color
           color="blue"
           setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
         />
         <Color
           color="green"
           setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
         />
       </div>
     </div>
