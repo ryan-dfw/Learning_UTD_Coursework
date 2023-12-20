0382358788c33bd92691f0e57ddf10b41052b858
Ryan <mckevitt.r@gmail.com>
2023-12-20 02:27:40 -0600

    b31 prepared mock server w/ routes for get, pets by name, & pets by owner

--- block31/index.js
+++ block31/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 const PORT = 8080;
 
+app.use(express.static("public"));
+
 // GET - / - returns homepage
 app.get("/", (req, res) => {
   // serve up the public folder as static index.html file
@@ -20,27 +22,23 @@ app.get("/api", (req, res) => {
 
 // get all pets from the database
 app.get("/api/v1/pets", (req, res) => {
-  // send the pets array as a response
+  res.send(pets);
 });
 
 // get pet by owner with query string
 app.get("/api/v1/pets/owner", (req, res) => {
-  // get the owner from the request
   const owner = req.query.owner;
-  // find the pet in the pets array
   const pet = pets.find((pet) => pet.owner === owner);
-  res.send(pet);
-  // send the pet as a response
+  if (pet) res.send(pet);
+  res.send("No pet found with that owner");
 });
 
 // get pet by name
 app.get("/api/v1/pets/:name", (req, res) => {
-  // get the name from the request
   const name = req.params.name;
-  // find the pet in the pets array
   const pet = pets.find((pet) => pet.name === name);
-
-  // send the pet as a response
+  if (pet) res.send(pet);
+  res.send("No pet found with that name");
 });
 
 app.listen(PORT, () => {
