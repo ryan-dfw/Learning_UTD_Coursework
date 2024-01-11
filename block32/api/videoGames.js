ac195935ba5ea9253d3c84c352a2639cfd747452
Ryan <mckevitt.r@gmail.com>
2024-01-11 02:13:39 -0600

    b34 patched all nonworking endpoints

---
+++ block32/api/videoGames.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+
+const REPLACE_ME = "HELP REPLACE ME!!!!";
+
+const {
+  getAllVideoGames,
+  getVideoGameById,
+  createVideoGame,
+  updateVideoGame,
+  deleteVideoGame,
+} = require("../db/videoGames");
+
+// GET - /api/video-games - get all video games
+router.get("/", async (req, res, next) => {
+  try {
+    const videoGames = await getAllVideoGames();
+    res.send(videoGames);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET - /api/video-games/:id - get a single video game by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const videoGame = await getVideoGameById(req.params.id);
+    res.send(videoGame);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST - /api/video-games - create a new video game
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await createVideoGame(req.body);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT - /api/video-games/:id - update a single video game by id
+router.put("/:id", async (req, res, next) => {
+  try {
+    const result = await updateVideoGame(req.params.id, req.body);
+    res.send("updated");
+  } catch (err) {
+    next(error);
+  }
+});
+
+// DELETE - /api/video-games/:id - delete a single video game by id
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const result = await deleteVideoGame(reg.params.id);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ block34/Juicebox/api/utils.js
@@ -0,0 +1,13 @@
+function requireUser(req, res, next) {
+  if (!req.user) {
+    next({
+      name: "UnauthorizedUserError",
+      message: "You must be logged in to perform this action!",
+    });
+  }
+  next();
+}
+
+module.exports = {
+  requireUser,
+};
